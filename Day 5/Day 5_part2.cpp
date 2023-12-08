b111c66a121ee70c1354bb207c43a10d2a8f8de0
Crazybbsaber <yeeeeeetcoooby@gmail.com>
2023-12-08 10:05:52 +0100

    Trying to work something out
    It still doesn't work

--- Day 5/Day 5_part2.cpp
+++ Day 5/Day 5_part2.cpp
@@ -7,7 +7,7 @@
 // I tried to run it, it took 25 minutes, 26 GB of RAM and it returned a blank line, I will do it one day...
 
 int main() {
-	std::fstream file("test.txt"); // I don't know why but this laptop decided that fstream will stop working...
+	std::fstream file("input.txt"); // I don't know why but this laptop decided that fstream will stop working...
 	std::string s;
 
 	// {std::cout << "This code will fuck your computer! Proceed? [y/n] ";
@@ -16,7 +16,6 @@ int main() {
 	// if (x == 'N' || x == 'n' || x == 13) return 0;}
 
 	std::vector<long long> seeds;
-	std::unordered_set<int> changed;
 
 	std::getline(file, s);
 	s = s.substr(7);
@@ -32,13 +31,10 @@ int main() {
 		s = s.substr(split+1);
 	}
 
-	for (int i = 0; i < seeds.size(); i += 2) {
-		std::cout << seeds[i] << '\t' << (seeds[i]+seeds[i+1]) << '\n';
-	}
-
 	while (std::getline(file, s)) {
 		if (s == "") continue;
 		if (s.back() == ':') {
+			// std::cout << "SIZE_" << seeds.size() << '\n';
 			continue;
 		}
 
@@ -48,17 +44,48 @@ int main() {
 		s = s.substr(s.find(' ')+1);
 		long long range = (std::stoll(s) - 1);
 
-		for (int i = 0; i < seeds.size(); i++) {
-			if (changed.find(i) != changed.end()) continue;
+		// std::cout << seeds.size() << std::endl;
 
-			if (seeds[i] <= start) {
-				if ((seeds[i]+seeds[i+1]) >= (start+range)) {
+		for (int i = 0; i < seeds.size(); i+=2) {
+			// std::cout << seeds[i] << ' ' << seeds[i+1] << '\t';
+
+			if (seeds[i] >= start && seeds[i] < (start+range)) {
+				if ((seeds[i]+seeds[i+1]) < (start+range)) {
 					seeds[i] = dest + (seeds[i] - start);
-					changed.insert(i);
-				} else if ((seeds[i]+seeds[i+1]) < (start+range)) {
-					// Later that day...
+				} else if ((seeds[i]+seeds[i+1]) >= (start+range)) {
+					long long dif = (seeds[i]+seeds[i+1])-(start+range);
+					seeds[i+1] -= dif;
+					seeds[i] = dest + (seeds[i] - start);
+					seeds.push_back(start+range+1);
+					seeds.push_back(dif);
+				}
+			} else if (seeds[i] < start) {
+				if ((seeds[i]+seeds[i+1]) > (start+range)) {
+					long long dif_front = start - seeds[i];
+					long long dif_back = (seeds[i]+seeds[i+1]) - (start+range);
+					seeds.push_back(seeds[i]);
+					seeds.push_back(dif_front-1);
+					seeds.push_back(seeds[i]+seeds[i+1]-dif_back);
+					seeds.push_back(dif_back-1);
+					seeds[i] += dif_front;
+					seeds[i+1] -= dif_back;
+					seeds[i] = dest;
+				} else if ((seeds[i]+seeds[i+1]) <= (start+range) && (seeds[i]+seeds[i+1]) > start) {
+					long long dif = start - seeds[i];
+					seeds.push_back(start);
+					seeds.push_back(dif-1);
+					seeds[i] += dif;
+					seeds[i-1] -= dif;
 				}
 			}
 		}
-	}	
+	}
+
+	long long min = LLONG_MAX;
+	for (int i = 0; i < seeds.size(); i+=2) min = std::min(min, seeds[i]);
+
+	std::cout << min << '\n';
+
+	for (int i = 0; i < seeds.size(); i++) std::cout << seeds[i] << ' ' << i << '\t';
+	std::cout << seeds.size() << '\n';
 }
